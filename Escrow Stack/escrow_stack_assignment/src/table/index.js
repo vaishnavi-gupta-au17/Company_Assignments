74c37b9c2fc6d1df8090ad8a27a944942c947d65
vaishnavi-gupta-au17 <84459539+vaishnavi-gupta-au17@users.noreply.github.com>
2024-07-17 16:51:28 +0530

    final

--- Escrow Stack/escrow_stack_assignment/src/table/index.js
+++ Escrow Stack/escrow_stack_assignment/src/table/index.js
@@ -10,55 +10,6 @@ import TableRow from "@mui/material/TableRow";
 import AccountData from "../companyAccount/AccountData";
 
 const TableData = ({ selectedAccount }) => {
-  // function createData(date, credit, Ac_Balance, utr_rrn, ac_upi) {
-
-  //   return { date, credit, Ac_Balance, utr_rrn, ac_upi };
-  // }
-
-  // const rows = [
-  //   createData(
-  //     "07/05/2024 01:04 PM",
-  //     "$21,337",
-  //     "$21,337",
-  //     "$21,337",
-  //     "AC0CF7RRUY407QHU"
-  //   ),
-  //   createData(
-  //     "04/05/2024 12:38 PM",
-  //     "$21,337",
-  //     "$21,337",
-  //     "CMS4136431811",
-  //     "AC0CF7RRUY407QHU"
-  //   ),
-  //   createData(
-  //     "28/03/2024 05:09 PM",
-  //     "$21,337",
-  //     "$21,337",
-  //     "CMS4136431811",
-  //     "0104SLNEFTPL"
-  //   ),
-  //   createData(
-  //     "28/05/2024 05:09 PM",
-  //     "$5,015.69",
-  //     "$5,015.69",
-  //     "CMS3956431731",
-  //     "ACT861VM9RR67Z5C"
-  //   ),
-  //   createData(
-  //     "19/03/2024 11:33 PM",
-  //     "$5,015.69",
-  //     "$5,015.69",
-  //     "CMS3956431984",
-  //     "AC0CF7RRUY407QHU"
-  //   ),
-  //   createData(
-  //     "19/03/2024 11:33 PM",
-  //     "$16,000",
-  //     "$16,000",
-  //     "CMS3956464916",
-  //     "ACT861VM9RR67Z5C"
-  //   ),
-  // ];
   const rows = AccountData[selectedAccount];
   return (
     <>
